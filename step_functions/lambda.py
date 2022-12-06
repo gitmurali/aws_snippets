c1f8593bfb6f2dfc6adeb5dec5813fc7404c9a99
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-12-06 18:03:43 +0000

    step_function_lambda

---
+++ step_functions/lambda.py
@@ -0,0 +1,8 @@
+import json
+
+def lambda_handler(event, context):
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('You have invoked lambda function from Step Function successfully.’)
+    }
