8bdc1f98fbfc747df97df251fb86aac576e375cb
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-12-06 18:28:53 +0000

    update resource arn

--- step_functions/lambda.py
+++ step_functions/lambda.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('You have invoked lambda function from Step Function successfully.’)
+        'body': json.dumps("You have invoked lambda function from Step Function successfully")
     }
